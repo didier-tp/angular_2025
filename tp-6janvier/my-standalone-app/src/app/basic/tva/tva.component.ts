06ebe9e60943843017bfdd6a348709d447f77edb
DidierDefrance <didier@d-defrance.fr>
2025-01-06 15:00:49 +0100

    s

--- tp-6janvier/my-standalone-app/src/app/basic/tva/tva.component.ts
+++ tp-6janvier/my-standalone-app/src/app/basic/tva/tva.component.ts
@@ -1,11 +1,26 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tva',
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './tva.component.html',
   styleUrl: './tva.component.scss'
 })
 export class TvaComponent {
+
+  tauxPossibles = [ 5.0 , 10.0 , 20.0];
+
+  ht /* number*/ = 0;
+  taux /* number*/  = 0; //taux en %
+
+  onCalculerTvaEtTtc(){
+    this.tva = this.ht * this.taux / 100; 
+    this.ttc = this.ht + this.tva;
+  }
+
+  tva /* number*/= 0;
+  ttc /* number*/  = 0;
 
 }
