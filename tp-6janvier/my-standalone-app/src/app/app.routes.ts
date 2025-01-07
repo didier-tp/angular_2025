ce262c89f1c01c23f5760a01be802ac336c5e10a
DidierDefrance <didier@d-defrance.fr>
2025-01-07 17:14:08 +0100

    s

--- tp-6janvier/my-standalone-app/src/app/app.routes.ts
+++ tp-6janvier/my-standalone-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BasicComponent } from './basic/basic.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ConversionComponent } from './conversion/conversion.component';
 
 export const routes: Routes = [
     { path: 'ngr-welcome', component: WelcomeComponent },
     { path: '', redirectTo: '/ngr-welcome', pathMatch: 'full'},
+    { path: 'ngr-conversion', component: ConversionComponent } ,
     { path: 'ngr-login', component: LoginComponent },
     { path: 'ngr-basic', component: BasicComponent },
     { path: 'ngr-m2', loadChildren: () => import('../m2/m2.module').then(m => m.M2Module) },
---
+++ tp-6janvier/my-standalone-app/src/app/conversion/conversion.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {  AsyncPipe, NgFor } from '@angular/common';
+import { Devise } from '../common/data/devise';
+import { DeviseService } from '../common/service/devise.service';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-conversion',
+  imports: [FormsModule, NgFor,AsyncPipe],
+  templateUrl: './conversion.component.html',
+  styleUrl: './conversion.component.scss'
+})
+export class ConversionComponent {
+  montant: number = 0;
+  codeDeviseSource: string = "?";
+  codeDeviseCible: string = "?";
+  montantConverti: number = 0;
+  montantConvertiObs$!: Observable<number> ;
+  listeDevises: Devise[] = []; //à choisir dans liste déroulante.
+
+  constructor(private _deviseService: DeviseService) { }
+
+  onConvertir() {
+    console.log("debut de onConvertir")
+
+    this.montantConvertiObs$ =  this._deviseService.convertir$(this.montant,
+      this.codeDeviseSource,
+      this.codeDeviseCible);
+    /*
+    this._deviseService.convertir$(this.montant,
+      this.codeDeviseSource,
+      this.codeDeviseCible)
+      .subscribe({
+        next: (res: number) => {
+          this.montantConverti = res;
+          console.log("resultat obtenu en différé")
+        },
+        error: (err) => { console.log("error:" + err) }
+      });
+      */
+    console.log("suite immédiate (sans attente) de onConvertir");
+    //Attention : sur cette ligne , le résultat n'est à ce stade pas encore connu
+    //car appel asynchrone non bloquant et réponse ultérieure via callback
+  }
+
+  initListeDevises(tabDevises: Devise[]) {
+    this.listeDevises = tabDevises;
+    if (tabDevises && tabDevises.length > 0) {
+      this.codeDeviseSource = tabDevises[0].code; //valeur par défaut
+      this.codeDeviseCible = tabDevises[0].code; //valeur par défaut
+    }
+  }
+
+  //ngOnInit() est automatiquement appelée par le framework après le constructeur
+  //et après la prise en compte des injections et des éventuels @Input
+  ngOnInit(): void {
+    this._deviseService.getAllDevises$()
+      .subscribe({
+        next: (tabDev: Devise[]) => { this.initListeDevises(tabDev); },
+        error: (err) => { console.log("error:" + err) }
+      });
+  }
+}
