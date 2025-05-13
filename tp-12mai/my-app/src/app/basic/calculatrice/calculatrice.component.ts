3a9e9c72d93a95df62431aa2db04d980569cf3a5
didier <didier@d-defrance.fr>
2025-05-13 11:29:08 +0200

    s

--- tp-12mai/my-app/src/app/basic/calculatrice/calculatrice.component.ts
+++ tp-12mai/my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-calculatrice',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculatrice.component.html',
   styleUrl: './calculatrice.component.scss'
 })
 export class CalculatriceComponent {
+  a: number = 0;
+  b: number = 0;
+  res: number = 0;
 
+  onCalculer(op: string) {
+    switch (op) {
+      case "+":
+        this.res = Number(this.a) + Number(this.b); break;
+      case "-":
+        this.res = this.a - this.b; break;
+      case "*":
+        this.res = this.a * this.b; break;
+      default:
+        this.res = 0;
+    }
+  }
+
+  //coordonnées relatives de la souris qui survole une div
+x:number=0;
+y:number=0;
+onMouseMove(evt : MouseEvent){
+let currentDiv : HTMLElement= <HTMLElement> evt.target;
+this.x = evt.pageX - currentDiv.offsetLeft;
+this.y = evt.pageY - currentDiv.offsetTop;
+}
+onMouseLeave(evt : MouseEvent){
+this.x=0; this.y=0;
+}
 }
