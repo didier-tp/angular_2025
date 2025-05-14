bb2e4423c7eb455ebea7031c9212b9c92226e997
Didier_Defrance <didier@d-defrance.fr>
2025-05-14 10:19:15 +0200

    s

--- tp-12mai/my-app/src/app/header/header.component.ts
+++ tp-12mai/my-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,8 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./header.component.scss','../common/style/common.scss']
 })
 export class HeaderComponent {
+
+  @Input()
+  titre = "titre_par_defaut"
 
 }
