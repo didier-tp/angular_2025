4281106b2ca2997faf07c9f4af168b4e8553dced
Didier_Defrance <didier@d-defrance.fr>
2025-05-14 09:01:59 +0200

    s

--- tp-12mai/my-app/src/app/app.routes.ts
+++ tp-12mai/my-app/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { BasicComponent } from './basic/basic.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 export const routes: Routes = [
     { path: 'ngr-welcome', component: WelcomeComponent },
     { path: '', redirectTo: '/ngr-welcome', pathMatch: 'full'},
     { path: 'ngr-login', component: LoginComponent },
     { path: 'ngr-basic', component: BasicComponent },
+    { path: 'ngr-reservation', component: ReservationComponent },
     { path: '**', redirectTo: '/ngr-welcome', pathMatch: 'full' }
 ];
