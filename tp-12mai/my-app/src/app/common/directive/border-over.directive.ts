16dce312947c19e86b387cff6c2323460b1424aa
Didier_Defrance <didier@d-defrance.fr>
2025-05-14 13:30:42 +0200

    s

--- tp-12mai/my-app/src/app/common/directive/border-over.directive.ts
+++ tp-12mai/my-app/src/app/common/directive/border-over.directive.ts
@@ -1,12 +1,44 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[borderOver]'
 })
 export class BorderOverDirective {
-
+  /*
   constructor(el: ElementRef) { 
     el.nativeElement.style.border = "1px solid red";
   }
+    */
+
+  @Input('borderOver')
+  set borderColor(borderColor:string){
+    //NB: if no value specified , default value of color is empty string
+    if(borderColor!="")
+     this._borderColor=borderColor;
+  }
+
+  @HostBinding('style.borderColor')
+  private _borderColor = 'red'; //default color
+
+  @HostBinding('style.borderStyle')
+  private _borderStyle = 'hidden'; 
+
+
+  constructor(el: ElementRef) {
+    //always sttings:
+    el.nativeElement.style._borderWidth = '2px';
+   }
+
+   @HostListener('mouseenter')
+   onMouseEnter(){
+       this._borderStyle = 'solid';
+   }
+
+   @HostListener('mouseleave')
+   onMouseLeave(){
+    //this._borderStyle = 'none';
+    this._borderStyle = 'hidden';
+   }
+
 
 }
