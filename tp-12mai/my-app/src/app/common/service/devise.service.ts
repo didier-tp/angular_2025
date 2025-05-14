65061031ba46a2f2194472c3498a168143151341
Didier_Defrance <didier@d-defrance.fr>
2025-05-14 17:44:00 +0200

    http

--- tp-12mai/my-app/src/app/common/service/devise.service.ts
+++ tp-12mai/my-app/src/app/common/service/devise.service.ts
@@ -1,33 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Devise } from '../data/devise';
 import { Observable, of } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+export interface ConvertRes {
+    source: string; //ex: "EUR",
+    target: string; //ex: "USD",
+    amount: number; //ex: 200.0
+    result: number; //ex: 217.3913
+};
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DeviseService {
-  //jeux de données (en dur) pour pré-version (simulation asynchrone)
-  private devises: Devise[] = [
-    new Devise('EUR', 'euro', 1.0),
-    new Devise('USD', 'dollar', 1.1),
-    new Devise('GBP', 'livre', 0.9)
-  ];
-  public getAllDevises$(): Observable<Devise[]> {
-    return of(this.devises) //version préliminaire (cependant asynchrone)
-      .pipe(
-        delay(111) //simuler une attente de 111ms
-      );
-  }
-  public convertir$(montant: number,
-    codeDeviseSrc: string,
-    codeDeviseTarget: string
-  ): Observable<number> {
-    let coeff = (codeDeviseSrc == codeDeviseTarget) ? 1 : Math.random();
-    //coefficient aleatoire ici (simple simulation)
-    let montantConverti = montant * coeff;
-    return of(montantConverti) //version temporaire (cependant asynchrone)
-      .pipe(
-        delay(222) //simuler une attente de 222ms
-      );
-  }
+    private _apiBaseUrl = "https://www.d-defrance.fr/tp/devise-api/v1";
+    constructor(private _http: HttpClient) { }
+    public getAllDevises$(): Observable<Devise[]> {
+        let url = this._apiBaseUrl + "/public/devises";
+        console.log("url = " + url);
+        return this._http.get<Devise[]>(url);
+    }
+    public convertir$(montant: number,
+        codeDeviseSrc: string,
+        codeDeviseTarget: string
+    ): Observable<number> {
+        const url = this._apiBaseUrl + "/public/convert"
+            + `?source=${codeDeviseSrc}`
+            + `&target=${codeDeviseTarget}&amount=${montant}`;
+        //console.log( "url = " + url);
+        return this._http.get<ConvertRes>(url)
+            .pipe(
+                map((res: ConvertRes) => res.result)
+            );
+    }
 }
