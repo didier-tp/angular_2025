a54efd7dbd1a761a0a35e817c51af616b412c203
Didier_Defrance <didier@d-defrance.fr>
2025-05-14 11:58:19 +0200

    s

--- tp-12mai/my-app/src/app/basic/basic.component.ts
+++ tp-12mai/my-app/src/app/basic/basic.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 import { TvaComponent } from './tva/tva.component';
 import { TogglePanelComponent } from '../common/component/toggle-panel/toggle-panel.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-basic',
-  imports: [CalculatriceComponent, TvaComponent,TogglePanelComponent],
+  imports: [CalculatriceComponent, TvaComponent,TogglePanelComponent,MatTabsModule],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.scss'
 })
