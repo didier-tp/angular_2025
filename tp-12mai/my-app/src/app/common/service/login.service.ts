908465f86678b45bbbc9077efa57289559b94211
Didier_Defrance <didier@d-defrance.fr>
2025-05-15 10:23:45 +0200

    s

--- tp-12mai/my-app/src/app/common/service/login.service.ts
+++ tp-12mai/my-app/src/app/common/service/login.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login, LoginResponse } from '../data/login';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  //private _apiBaseUrl ="https://www.d-defrance.fr/tp/standalone-login-api/v1";
+private _apiBaseUrl ="tp/standalone-login-api/v1";
+
+  constructor(private _http: HttpClient) { }
+
+  public postLogin$(login: Login): Observable<LoginResponse>{
+    let url =this._apiBaseUrl +"/public/auth";
+    return this._http.post<LoginResponse>(url,login);
+  }
 }
