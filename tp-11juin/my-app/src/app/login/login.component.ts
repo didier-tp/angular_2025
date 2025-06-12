509b915eb40a68843845e848c09e888ecd0cb2c4
Didier_Defrance <didier@d-defrance.fr>
2025-06-12 12:00:40 +0200

    s

--- tp-11juin/my-app/src/app/login/login.component.ts
+++ tp-11juin/my-app/src/app/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Login } from '../common/data/login';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  public login : Login = new Login();
+public message :string ="";
+public onLogin(){
+this.message = "donnees saisies = " + JSON.stringify(this.login);
+}
 }
