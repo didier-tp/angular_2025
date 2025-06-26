dd4c6f085118f64f2eeb964d149903ac2094d9b2
Didier_Defrance <didier@d-defrance.fr>
2025-06-26 11:59:28 +0200

    s

--- tp-11juin/my-app/src/app/common/service/login.service.ts
+++ tp-11juin/my-app/src/app/common/service/login.service.ts
@@ -8,8 +8,9 @@ import { Login, LoginResponse } from '../data/login';
 })
 export class LoginService {
 
-  baseUrl="https://www.d-defrance.fr/tp/standalone-login-api/v1"
-  //ou bien  baseUrl="tp/standalone-login-api/v1" si proxy.conf.json
+  //baseUrl="https://www.d-defrance.fr/tp/standalone-login-api/v1"
+  //ou bien  
+  baseUrl="tp/standalone-login-api/v1"// si proxy.conf.json
 
   constructor(private _http: HttpClient) { }
 
